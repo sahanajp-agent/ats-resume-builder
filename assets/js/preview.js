ef6db66aa5736238a8e85500d45b4e7cbe5f5d36
sahanajp-agent <sahana.jp@phronetic.ai>
2026-01-09 12:40:09 +0530

    Update preview.js

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -1,1 +1,9 @@
+const data = JSON.parse(localStorage.getItem("resumeData"));
+
+if (data) {
+  document.getElementById("r-name").innerText = data.name;
+  document.getElementById("r-contact").innerText =
+    `${data.email} | ${data.phone}`;
+  document.getElementById("r-summary").innerText = data.summary;
+}
 
