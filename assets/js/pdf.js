a37fe98353e8500e21f7d9a906d7eebbf08be4ce
sahanajp-agent <sahana.jp@phronetic.ai>
2026-01-09 13:26:05 +0530

    Update pdf.js

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -1,1 +1,9 @@
+function downloadPDF() {
+  const element = document.getElementById("resumeContent");
+
+  html2pdf().from(element).set({
+    filename: "ATS_Resume.pdf",
+    jsPDF: { format: "a4", orientation: "portrait" }
+  }).save();
+}
 
